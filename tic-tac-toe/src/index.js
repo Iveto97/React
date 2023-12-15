fff51b068c503226e2d370f7372d30c730653007
Iva Tsvetanova <141308984+Iveto97@users.noreply.github.com>
2023-12-15 21:13:03 +0200

    First small tic-tac-toe game

---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+
+import './App.css';
+
+import Game from './App.js'
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<Game />)
+
